3572667b20fa58314a1ed226f725538c10b7b2ff
tonyhacq <34024678+tonyhacq@users.noreply.github.com>
2017-12-04 10:20:29 +0100

    Add files via upload

---
+++ login/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+# Register your models here.
+# from .models import user
+
+# class userAdmin(admin.ModelAdmin):
+# 	class Meta:
+# 		model = user
+
+# admin.site.register(user, userAdmin)
---
+++ login/models.py
@@ -0,0 +1,14 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+# class user(models.Model):
+# 	username = models.CharField(max_length=120)
+# 	password = models.CharField(max_length=120)
+# 	email = models.EmailField(max_length=120)
+# 	firstname = models.CharField(max_length=120)
+# 	lastname = models.CharField(max_length=120)
+	
+# 	def __str__(self):
+# 		return self.username
