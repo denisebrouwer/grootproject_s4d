8a0f82b29445c1134552f9a3bb8d008e2aba8c86
denisebrouwer <33348366+denisebrouwer@users.noreply.github.com>
2017-12-13 10:07:27 +0100

    Update urls.py

--- urls.py
+++ urls.py
@@ -20,7 +20,7 @@ from django.contrib import admin
 
 from profiles import views as profiles_views
 from contact import views as contact_views
-from login.views import (login_view, register_view, logout_view)
+from login.views import (login_view, register_view, logout_view, profile_edit) 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -28,11 +28,11 @@ urlpatterns = [
     url(r'^about/$', profiles_views.about, name='about'),
     url(r'^instellingen/$', profiles_views.instellingen, name='instellingen'),
     url(r'^voorkeurlijst/$', profiles_views.voorkeurlijst, name='voorkeurlijst'),
-    url(r'^infolijst/$', profiles_views.infolijst, name='infolijst'),
     url(r'^contact/$', contact_views.contact, name='contact'),
     url(r'^login/$', login_view, name='login'),
     url(r'^logout/$', logout_view, name='logout'),
     url(r'^register/$', register_view, name='register'),
+    url(r'^register/profile/$', profile_edit, name='profile'),
 ]
  
 if settings.DEBUG:
