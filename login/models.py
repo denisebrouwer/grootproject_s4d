15daec4fd548351220237b8548b34c5dce3e0010
denisebrouwer <33348366+denisebrouwer@users.noreply.github.com>
2017-12-13 10:10:07 +0100

    Update models.py

--- login/models.py
+++ login/models.py
@@ -1,14 +1,20 @@
 from __future__ import unicode_literals
 
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
 
-# Create your models here.
-# class user(models.Model):
-# 	username = models.CharField(max_length=120)
-# 	password = models.CharField(max_length=120)
-# 	email = models.EmailField(max_length=120)
-# 	firstname = models.CharField(max_length=120)
-# 	lastname = models.CharField(max_length=120)
-	
-# 	def __str__(self):
-# 		return self.username
+class Profile(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	favfood = models.CharField(max_length=100, default='')
+	hatefood = models.CharField(max_length=100, default='')
+	woonplaats = models.CharField(max_length=100, default='')
+
+	def __str__(self):
+		return self.user.username
+
+def create_profile(sender, **kwargs):
+	if kwargs['created']:
+		profile  = Profile.objects.create(user=kwargs['instance'])
+
+post_save.connect(create_profile, sender=User) 
